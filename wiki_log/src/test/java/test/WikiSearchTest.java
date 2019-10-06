80088da848938adb46d0576f7b4a3471689a8589
Lizamil <54038873+Lizamil@users.noreply.github.com>
2019-10-07 00:40:14 +0300

    refresh7

--- wiki_log/src/test/java/test/WikiSearchTest.java
+++ wiki_log/src/test/java/test/WikiSearchTest.java
@@ -12,7 +12,6 @@ public class WikiSearchTest extends TestBase {
         String search = "gaGARin";
         app.getSearchHelper().searchInWiki(search);
         Assert.assertTrue(app.getSearchHelper().firstHeadingContainsSearchWord(search));
-
             }
 }
 
