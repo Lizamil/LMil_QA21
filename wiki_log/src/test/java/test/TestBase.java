ab5635b6af098b877364d55dd6d7bc6de46c5b91
Lizamil <54038873+Lizamil@users.noreply.github.com>
2019-10-07 23:54:52 +0300

    add log

--- wiki_log/src/test/java/test/TestBase.java
+++ wiki_log/src/test/java/test/TestBase.java
@@ -2,12 +2,19 @@ package test;
 
 import manager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import java.lang.reflect.Method;
 
 public class TestBase {
 
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeSuite
     public void setUP() {
@@ -20,5 +27,15 @@ public class TestBase {
         app.stop();
     }
 
+    @BeforeMethod
+    public void startLogger(Method m) { //, Object[] p
+        logger.info("start test " + m.getName() ); //+ " with parameters " + Arrays.asList(p));
+    }
 
+    @AfterMethod
+    public void stopLogger(Method m) {
+        logger.info("stop test " + m.getName());
+        System.out.println("________________________________________________________________");
+
+    }
 }
